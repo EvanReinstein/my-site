6d7ce37f8f4e07b7bf086c86865e562cb2b12c37
EvanReinstein <edreins@gmail.com>
2019-01-31 14:56:40 -0800

    fixes images and adds styling to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Icon } from 'react-icons-kit';
+import { githubSquare } from 'react-icons-kit/fa/githubSquare';
 
 const Projects = (props) => {
   const featured = props.data.filter(project => project.featured === true);
@@ -14,18 +15,15 @@ const Projects = (props) => {
       </header>
       <section>
         {featured.map(project => (
-          <div>
-            <article key={project.title} className="project featured">hello</article>
             <article key={project.title} className="project featured">
-              <h2>{project.title}</h2><span>{project.date}</span>
               <img src={`${project.image}`} alt="project image"/>
+              <h2>{project.title}</h2><span>{project.date}</span>
               <p>{project.desc}</p>
-              <ul>
+              <ul className="tech-list">
                 {project.tech.map(tech => <li className="tech">{tech}</li>)}
               </ul>
-              <p>{project.repo}</p>
+              <a href={`${project.repoLink}`} target="_blank" rel="noopener noreferrer"><Icon icon={githubSquare} /></a>
             </article>
-          </div>
         ))}
         {nonfeatured.map(project => (
           <article key={project.title} className="project">nonfeatured</article>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,13 @@
+import crow from '../images/crow.png';
+import logo from '../images/meenstream-logo.png';
+import wayfarer from '../images/sf.jpg';
+
 const data  = [
   {
   title: "Crow Watch",
   desc: "An app for keeping an eye on my deck, in order to prevent crows from making it dirty!",
   tech: ["React Native", "Expo", "AWS: Amplify", "AWS: Cognito", "AWS: S3", "AWS: IAM"],
-  image: "../images/crow.png",
+  image: crow,
   repoLink: "https://github.com/EvanReinstein/crow-watch-front-end",
   date: "December 2018",
   featured: true,
@@ -12,7 +16,7 @@ const data  = [
     title: "Meenstream",
     desc: "An app built with Python and Django to experiment with the Spotify API",
     tech: ["Python", "Django", "PostgresQL", "CSS3", "Heroku"],
-    image: "../images/meenstream-logo.png",
+    image: logo,
     repoLink: "https://github.com/EvanReinstein/meenstream",
     date: "October 2018",
     featured: true,
@@ -21,7 +25,7 @@ const data  = [
     title: "Wayfarer",
     desc: "An app for sharing travel tips",
     tech: ["React", "Mongoose", "Express", "Node.js", "MongoDB", "Bootstrap", "JSON Web Token", "BCrypt"],
-    image: "../images/sf.jpg",
+    image: wayfarer,
     repoLink: "https://git.generalassemb.ly/EvanReinstein/wayfarer-front-end",
     date: "November 2018",
     featured: false,
