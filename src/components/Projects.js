9ccffe57bb03fe4164f777d824087a162c4c51a6
EvanReinstein <edreins@gmail.com>
2019-01-31 14:41:39 -0800

    Adds project images.  Changes project data keys.  Renders projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 const Projects = (props) => {
-
   const featured = props.data.filter(project => project.featured === true);
   const nonfeatured = props.data.filter(project => project.featured !== true);
-  console.log(featured);
 
   return (
     <div className="projects">
@@ -13,10 +13,34 @@ const Projects = (props) => {
         <p>Check out some of the things I've built</p>
       </header>
       <section>
-
+        {featured.map(project => (
+          <div>
+            <article key={project.title} className="project featured">hello</article>
+            <article key={project.title} className="project featured">
+              <h2>{project.title}</h2><span>{project.date}</span>
+              <img src={`${project.image}`} alt="project image"/>
+              <p>{project.desc}</p>
+              <ul>
+                {project.tech.map(tech => <li className="tech">{tech}</li>)}
+              </ul>
+              <p>{project.repo}</p>
+            </article>
+          </div>
+        ))}
+        {nonfeatured.map(project => (
+          <article key={project.title} className="project">nonfeatured</article>
+        ))}
       </section>
     </div>
   );
+};
+
+Projects.propTypes = {
+  data: PropTypes.array.isRequired,
+};
+
+Projects.defaultProps = {
+  data: [],
 };
 
 export default Projects;
