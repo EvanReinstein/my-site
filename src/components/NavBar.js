e376787e8d4c1f4035d80a6d9054a59560c574a3
EvanReinstein <edreins@gmail.com>
2019-02-01 14:12:24 -0800

    Adds hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import SideMenu from './HamburgerMenu';
 
 const Navbar = () => {
     return (
         <nav className="main-nav">
-          <h1>
-            <NavLink to="/" exact>Evan Reinstein</NavLink>
-          </h1>
-          <ul className="links">
-            <li><NavLink className="link" to="/about">About</NavLink></li>
-            <li><NavLink className="link" to="/projects">Projects</NavLink></li>
-            <li><NavLink className="link" to="/experience">Experience</NavLink></li>
-            <li><NavLink className="link last" to="/contact">Contact</NavLink></li>
-          </ul>
+          <div className="nav-wrap">
+            <h1>
+              <NavLink to="/" exact>Evan Reinstein</NavLink>
+            </h1>
+            <ul className="links">
+              <li><NavLink className="link" to="/about">About</NavLink></li>
+              <li><NavLink className="link" to="/projects">Projects</NavLink></li>
+              <li><NavLink className="link" to="/experience">Experience</NavLink></li>
+              <li><NavLink className="link last" to="/contact">Contact</NavLink></li>
+            </ul>
+          </div>
+          <div className="wrap">
+            <div className="hamburger">
+              <SideMenu />
+            </div>
+          </div>
         </nav>
     )
 }
