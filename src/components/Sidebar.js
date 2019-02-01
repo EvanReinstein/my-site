ab6fb227fcde5f3fd6bd1645fcee50812fbbb8b2
EvanReinstein <edreins@gmail.com>
2019-02-01 11:19:52 -0800

    Links contact and email to mailto:

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,7 +7,7 @@ const Sidebar = () => {
     <div className="sidebar">
       <section className="header">
         <h1>Evan Reinstein</h1>
-        <p>edreins@gmail.com</p>
+        <p><a href="mailto:edreins@gmail.com" className="linked-text">edreins@gmail.com</a></p>
       </section>
       <section className="sidebar-blurb">
         <p>Hey, I'm Evan, a former middle school math teacher turned Web Developer.  Right now I'm working on building custom Alexa skills to learn about AWS and Lambda Functions.</p>
